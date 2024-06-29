0df8214b67e0530f5578fecdc741d75b3c380a66
FIRST_NAME LAST_NAME <ppavle@live.com>
2024-06-29 15:53:35 +0200

    GetAllUsers route

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
+import { User } from "../models/User";
 
 export const getUsers = async (req: Request, res: Response) => {
-  res.send("Get Users");
+  try {
+    const users = await User.find({ select: ["id", "fullName", "email", "created_at", "updated_at"] });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return res.status(500).json({ error: "Failed to fetch users" });
+  }
 };
